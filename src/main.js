c070c706b71c4647aed07d70ed98654b6ba377e4
hukai <huzhengen@163.com>
2019-08-27 08:57:04 +0800

    配置babel.config.js以及添加element-ui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 
+import { Button } from 'element-ui';
+
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
-	router,
-	render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
