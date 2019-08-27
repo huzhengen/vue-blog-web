5ec22d82e149498d495a75ecd161a8cb46f55201
hukai <huzhengen@163.com>
2019-08-27 11:11:00 +0800

    调试登陆接口

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,20 +10,6 @@
 <script>
 export default {
     name: 'Header',
-    props: {
-        msg: String
-    },
-    data() {
-        return {
-            activeIndex: '1',
-            activeIndex2: '1'
-        };
-    },
-    methods: {
-        handleSelect(key, keyPath) {
-            console.log(key, keyPath);
-        }
-    }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,30 +1,45 @@
 <template>
     <div class="login">
-        <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="用户名：">
-                <el-input v-model="form.name"></el-input>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="用户名：" prop="username">
+                <el-input v-model="ruleForm.username"></el-input>
             </el-form-item>
-            <el-form-item label="密码；">
-                <el-input v-model="form.password"></el-input>
+            <el-form-item label="密码；" prop="password">
+                <el-input v-model="ruleForm.password"></el-input>
             </el-form-item>
-            <el-button>登陆</el-button>
+            <el-button @click="onSubmit">登陆</el-button>
         </el-form>
     </div>
 </template>
 <script>
+import request from '../helper/request'
 export default {
     name: 'Login',
     data() {
         return {
-            form: {
-                name: '',
+            ruleForm: {
+                username: '',
                 password: '',
+            },
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                    { min: 1, max: 15, message: '长度在 1 到 15 个字符', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                    { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
+                ],
             }
         }
     },
     methods: {
         onSubmit() {
-            console.log('submit!');
+            let username = this.ruleForm.username;
+            let password = this.ruleForm.password;
+            request('/users/login', 'post', { username, password }).then(res => {
+                this.$router.push({ path: this.$route.query.redirect || '/' })
+            })
         }
     }
 }
