cde55cde6b27b7716aedc83424421b8221f4cfe7
hukai <huzhengen@163.com>
2019-09-06 16:36:58 +0800

    Update Article.vue

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,18 +1,31 @@
 <template>
-    <div class="article">
-        <h1>article</h1>
-        <router-link :to="`/`">
-            index
-        </router-link>
-    </div>
+  <div class="article">
+    <h1>{{blog.title}}</h1>
+    <p>{{blog.content}}</p>
+  </div>
 </template>
 <script>
+import request from "../helper/request";
 export default {
-    name: 'Article',
-    props: {
-        msg: String
+  name: "Article",
+  data() {
+    return {
+      blog: {}
+    };
+  },
+  created() {
+    this.getBlog();
+  },
+  methods: {
+    getBlog() {
+      let blogId = this.$route.params.blogId;
+      request(`/blogs/${blogId}`, "get").then(res => {
+        console.log(res);
+        this.blog = res.data;
+      });
     }
-}
+  }
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
