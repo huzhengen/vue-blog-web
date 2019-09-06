bc6dcf68492185655039f259e3587cfebc018763
hukai <huzhengen@163.com>
2019-09-06 16:37:25 +0800

    Update Index.vue

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,42 +1,43 @@
 <template>
-    <div class="index">
-        <h1>所有博客</h1>
-        <ul>
-            <li v-for="blog in blogs" :key="blog.id">
-                <router-link :to="`/blog/${blog.id}`">{{blog.title}}</router-link>
-            </li>
-        </ul>
-    </div>
+  <div class="index">
+    <h1>所有博客</h1>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.id">
+        <router-link :to="`/blog/${blog.id}`">{{blog.title}}</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
-import request from '../helper/request'
+import request from "../helper/request";
 export default {
-    name: 'Index',
-    data() {
-        return {
-            blogs: []
-        }
-    },
-    created() {
-        this.getBlogs();
-    },
-    methods: {
-        getBlogs() {
-            request('/blogs', 'get').then(res => {
-                this.blogs = res.data;
-            })
-        }
+  name: "Index",
+  data() {
+    return {
+      blogs: []
+    };
+  },
+  created() {
+    this.getBlogs();
+  },
+  methods: {
+    getBlogs() {
+      request("/blogs", "get").then(res => {
+        this.blogs = res.data;
+      });
     }
-}
+  }
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 ul,
 li {
-    list-style: none;
-    margin: 0;
-    padding: 0;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 
-.index {}
+.index {
+}
 </style>
